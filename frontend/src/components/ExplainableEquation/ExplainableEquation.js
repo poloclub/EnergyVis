eb2df4be89da0bdabfc1a17e16dccb8bd2039a24
Omar Shaikh <oshaikh13@gmail.com>
2021-01-08 11:26:54 -0500

    rearrange eqs

--- frontend/src/components/ExplainableEquation/ExplainableEquation.js
+++ frontend/src/components/ExplainableEquation/ExplainableEquation.js
@@ -58,9 +58,56 @@ class ExplainableEquation extends React.PureComponent {
           aria-labelledby="nested-list-subheader"
           style={{width: '100%'}}
         >
+        <ListItem button onClick={() => this.handleClick("eq3")}>
+            <ListItemText>
+              CO<sub>2</sub> Emissions
+            </ListItemText>
+            <ListItemText>
+              <BlockMath>{String.raw`\textcolor{${colors[1]}}{\mathrm{CO}_{2} \mathrm{e}}= \textcolor{${colors[4]}}{${NRELData[(TrackerStore.hoveredState || TrackerStore.initialState)]["co2_lb_kwh"].toFixed(2)}} \cdot \textcolor{${colors[5]}}{\overline{p_{epoch}}}`}</BlockMath>
+            </ListItemText>
+            {this.state["eq3"] ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={this.state["eq3"]} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItem>
+                <ListItemText>
+                <Typography style={{paddingBottom: '2%'}} variant="body1">
+                  To convert to <span style={{fontWeight: 600, color: colors[1]}}>CO<sub>2</sub> emissions</span>, in pounds, we multiply the
+                  <span style={{fontWeight: 600, color: colors[4]}}> state-wide coefficient for lb of CO<sub>2</sub></span> produced per kilowatt hour consumed (from NREL) by 
+                  the <span style={{fontWeight: 600, color: colors[5]}}>power consumed during an epoch p<sub>epoch</sub></span>. 
+                </Typography>
+                </ListItemText>
+              </ListItem>
+            </List>
+          </Collapse>
+          <ListItem button onClick={() => this.handleClick("eq2")}>
+            {/* <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon> */}
+            <ListItemText>
+              Epoch Power Consumption
+            </ListItemText>
+            <ListItemText>
+              <BlockMath>{String.raw`\textcolor{${colors[5]}}{\overline{p_{epoch}}} = \frac{\textcolor{${colors[7]}}{\sum_{i=1}^{I}} \textcolor{${colors[0]}}{p_{i}}}{\textcolor{${colors[7]}}{I}} \cdot \frac{t_{epoch}}{1000}`}</BlockMath>
+            </ListItemText>
+            {this.state["eq2"] ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={this.state["eq2"]} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItem>
+                <ListItemText>
+                <Typography variant="body1">
+                  To find the <span style={{fontWeight: 600, color: colors[5]}}>power consumed over an epoch p<sub>epoch</sub></span> in kilowatt hours, 
+                  we take the <span style={{fontWeight: 600, color: colors[7]}}>average of all</span> your <span style={{fontWeight: 600, color: colors[0]}}>total instantaneous power usages p<sub>i</sub> </span> 
+                  during this epoch, multiply by the <span style={{fontWeight: 600}}>duration of the epoch in hours t<sub>epoch</sub></span>, and divide by <span style={{fontWeight: 600}}>1000</span>.
+                </Typography>
+                </ListItemText>
+              </ListItem>
+            </List>
+          </Collapse>
           <ListItem button onClick={() => this.handleClick("eq1")}>
           <ListItemText>
-              1. Total Instantaneous Power
+              Total Instantaneous Power
             </ListItemText>
             <ListItemText>
               <BlockMath>{String.raw`\textcolor{${colors[0]}}{p_{i}} = (\textcolor{${colors[3]}}{p_{chipset}} + \textcolor{${colors[2]}}{\sum_{g=1}^{G} p_{g}} \thinspace ) \cdot \textcolor{${colors[6]}}{${TrackerStore.initialPUE.toFixed(2)}} \hspace{3pt}`}</BlockMath>
@@ -97,52 +144,6 @@ class ExplainableEquation extends React.PureComponent {
                       </div> <u contentEditable="true">{TrackerStore.initialPUE.toFixed(2)}</u> [Ascierto 2020])</span> that adjusts for electricity used by other resources like cooling and lighting.
 
                   </Typography>
-                </ListItemText>
-              </ListItem>
-            </List>
-          </Collapse>
-          <ListItem button onClick={() => this.handleClick("eq2")}>
-            {/* <ListItemIcon>
-              <InboxIcon />
-            </ListItemIcon> */}
-            <ListItemText>
-              2. Epoch Power Consumption
-            </ListItemText>
-            <ListItemText>
-              <BlockMath>{String.raw`\textcolor{${colors[5]}}{\overline{p_{epoch}}} = \frac{\textcolor{${colors[7]}}{\sum_{i=1}^{I}} \textcolor{${colors[0]}}{p_{i}}}{\textcolor{${colors[7]}}{I}} \cdot \frac{t_{epoch}}{1000}`}</BlockMath>
-            </ListItemText>
-            {this.state["eq2"] ? <ExpandLess /> : <ExpandMore />}
-          </ListItem>
-          <Collapse in={this.state["eq2"]} timeout="auto" unmountOnExit>
-            <List component="div" disablePadding>
-              <ListItem>
-                <ListItemText>
-                <Typography variant="body1">
-                  To find the <span style={{fontWeight: 600, color: colors[5]}}>power consumed over an epoch</span> in kilowatt hours, 
-                  we take the <span style={{fontWeight: 600, color: colors[7]}}>average of all</span> our <span style={{fontWeight: 600, color: colors[0]}}>instantaneous measurements </span> 
-                  during this epoch, multiply by the <span style={{fontWeight: 600}}>duration of the epoch in hours</span>, and divide by <span style={{fontWeight: 600}}>1000</span>.
-                </Typography>
-                </ListItemText>
-              </ListItem>
-            </List>
-          </Collapse>
-          <ListItem button onClick={() => this.handleClick("eq3")}>
-            <ListItemText>
-              3. CO<sub>2</sub> Emissions
-            </ListItemText>
-            <ListItemText>
-              <BlockMath>{String.raw`\textcolor{${colors[1]}}{\mathrm{CO}_{2} \mathrm{e}}= \textcolor{${colors[4]}}{${NRELData[(TrackerStore.hoveredState || TrackerStore.initialState)]["co2_lb_kwh"].toFixed(2)}} \cdot \textcolor{${colors[5]}}{\overline{p_{epoch}}}`}</BlockMath>
-            </ListItemText>
-            {this.state["eq3"] ? <ExpandLess /> : <ExpandMore />}
-          </ListItem>
-          <Collapse in={this.state["eq3"]} timeout="auto" unmountOnExit>
-            <List component="div" disablePadding>
-              <ListItem>
-                <ListItemText>
-                <Typography style={{paddingBottom: '2%'}} variant="body1">
-                  Finally, to convert to <span style={{fontWeight: 600, color: colors[1]}}>CO<sub>2</sub> emissions</span>, in pounds, we multiply <span style={{fontWeight: 600, color: colors[5]}}>power consumed during an epoch</span> by a <span style={{fontWeight: 600, color: colors[4]}}>state-wide coefficient for lb of CO<sub>2</sub></span> produced per kilowatt hour consumed. 
-                  This coefficient is provided by projections from NREL. 
-                </Typography>
                 </ListItemText>
               </ListItem>
             </List>
