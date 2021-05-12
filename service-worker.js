7277a085c51a93c4f4edce57d6fa4981e35d49b7
oshaikh13 <oshaikh13@gmail.com>
2021-05-12 23:56:24 +0000

    Deploying to gh-pages from @ poloclub/EnergyVis@124ceebec094b2f186498a1984a2d0c6c8c19ec4 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b79b367223a54e9450e171a5fabe83a0.js"
+  "https://poloclub.github.io/EnergyVis/precache-manifest.57af93694262ced0b574d4707d177759.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://poloclub.github.io/EnergyVis/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
